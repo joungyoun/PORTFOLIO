0fb8024899fd57d5f56072659a9a32bcef2c9dc0
heo joung youn <h_jyy@naver.com>
2021-11-22 17:18:36 +0900

    CSS Responsive

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,13 +1,12 @@
 'use strict'
 
+//Make navbar transparent when it is on the top
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     console.log(window.scrollY);
     console.log('navbarHeight: ${nabarHeight}');
-    if(window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark')
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
+    
 });
+
+//Handle scrolling when tapping on the navbar navbar__menu
