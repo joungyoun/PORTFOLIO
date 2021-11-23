314351bd1b17fdb06663a7768b0286b224f51337
heo joung youn <h_jyy@naver.com>
2021-11-23 15:44:51 +0900

    JS Transparent

--- portfolio/main.js
+++ portfolio/main.js
@@ -33,7 +33,16 @@ homeContactBtn.addEventListener('click', () => {
     scrollIntoView('#contact')
 });
 
+//Make home slowly fade to transparent as the window scroll dowm
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+
 function scrollIntoView(selector) {
     const scrollTo = document.querySelector(selector);
     scrollTo.scrollIntoView({ behavior: 'smooth' });
 }
+
