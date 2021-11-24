6240281c0acf9b1ebe62eab1107665cb44844199
heo joung youn <h_jyy@naver.com>
2021-11-24 17:08:49 +0900

    JS remove active from nav

--- portfolio/main.js
+++ portfolio/main.js
@@ -50,9 +50,42 @@ document.addEventListener('scroll', () => {
     }
 });
 
-//Handle click on th "arrow up" button
+//Handle click on the "arrow up" button
 arrowUp.addEventListener('click', () => {
     scrollIntoView('#Home');
+});
+
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+    const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+    if(filter == null) {
+        return;
+    }
+
+// Remove selection from the previous item and select the new one
+    const active = document.querySelector('.category__btn.selected');
+    active.classList.remove('selected');
+    const target = 
+    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
+    target.classList.add('selected');
+
+
+    projectContainer.classList.add('anim-out');
+    setTimeout(() => {
+        projects.forEach((project) => {
+            console.log(project.dataset.type);
+            if (filter === '*' || filter === project.dataset.type) {
+                project.classList.remove('invisible');
+            }else {
+                project.classList .add('invisible');
+            }
+        });
+        projectContainer.classList.remove('anim-out');
+    }, 300);
 });
 
 function scrollIntoView(selector) {
