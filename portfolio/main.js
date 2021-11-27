d39fc6e7e614f5a750850a51c7cbf2414295b7ab
joungyoun <93749577+joungyoun@users.noreply.github.com>
2021-11-27 16:54:47 +0900

    JS project Filtering

--- portfolio/main.js
+++ portfolio/main.js
@@ -68,30 +68,21 @@ const projectContainer = document.querySelector('.work__projects');
 const projects = document.querySelectorAll('.project');
 workBtnContainer.addEventListener('click', (e) => {
     const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
-    if(filter == null) {
+    if (filter == null) {
         return;
     }
-
-// Remove selection from the previous item and select the new one
-    const active = document.querySelector('.category__btn.selected');
-    active.classList.remove('selected');
-    const target = 
-    e.target.nodeName === 'BUTTON' ? e.target : e.target.parentNode;
-    target.classList.add('selected');
-
-
     projectContainer.classList.add('anim-out');
     setTimeout(() => {
         projects.forEach((project) => {
             console.log(project.dataset.type);
             if (filter === '*' || filter === project.dataset.type) {
                 project.classList.remove('invisible');
-            }else {
-                project.classList .add('invisible');
+            } else {
+                project.classList.add ('invisible');
             }
         });
         projectContainer.classList.remove('anim-out');
-    }, 300);
+    }, 300);      
 });
 
 function scrollIntoView(selector) {
